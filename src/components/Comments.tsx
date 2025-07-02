a148c9a72ca3921db2e6fccb38b26ccaf44e1e2d
wagehzaietr <bad1game1@hotmail>
2025-07-02 23:26:17 +0400

    added new features

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -3,29 +3,9 @@ import Courses from './Courses'
 import Progress from './Progress'
 import toast from 'react-hot-toast'
 import { useState } from 'react'
+import {users} from '../data/Users'
 
 
-const users = [
-  {
-    name: 'Student Name: Mustafa Fathi :)',
-    comment: 'Comment 1',
-    image: '/user1.avif',
-    createdat: '2022-01-01'
-  },
-  {
-    name: 'Student Name: Mustafa Fathi :)',
-    comment: 'lorem ipsum ',
-    image: '/user2.avif',
-    createdat: '2022-01-01'
-  },
-  {
-    name: 'Student Name: Mustafa Fathi :)',
-    comment:
-      ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, dolores.',
-    createdat: '2022-01-01',
-    image: '/user3.avif'
-  }
-]
 
 function Comments () {
   const [input, setInput] = useState('')
@@ -42,6 +22,7 @@ function Comments () {
       {users.map((user, i) => (
         <div key={i} className='flex mt-4 items-start gap-4'>
           <img
+            loading='lazy'
             src={user.image}
             alt={user.name}
             className='w-20 h-20 rounded-full object-cover'
---
+++ src/data/Users.ts
@@ -0,0 +1,21 @@
+export const users = [
+  {
+    name: 'Student Name: Mustafa Fathi :)',
+    comment: 'Comment 1',
+    image: '/user1.avif',
+    createdat: '2022-01-01'
+  },
+  {
+    name: 'Student Name: Mustafa Fathi :)',
+    comment: 'lorem ipsum ',
+    image: '/user2.avif',
+    createdat: '2022-01-01'
+  },
+  {
+    name: 'Student Name: Mustafa Fathi :)',
+    comment:
+      ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, dolores.',
+    createdat: '2022-01-01',
+    image: '/user3.avif'
+  }
+]
