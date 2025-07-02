60a99d596b605f3cfd9caf0b23dfed4fd3f17036
wagehzaietr <bad1game1@hotmail>
2025-07-02 23:32:14 +0400

    added new features

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-
-import { lazy } from 'react'
+import { lazy, Suspense } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 
 const Home = lazy(() => import('./components/Home'))
 
-function App() {
-
-
+function App () {
   return (
     <>
-      <Navbar/>
-    <div className='bg-white/20 h-screen'>
-    <Home/>
-    </div>
+      <Navbar />
+      <div className='bg-white/20 h-screen'>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Home />
+        </Suspense>
+      </div>
     </>
   )
 }
